0a1039501f27c1fee1a78e7bff1d35a9ef4038e1
josh <jhell85@gmail.com>
2020-04-03 12:44:00 -0700

    added fade out of background function when form is submitted

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
     event.preventDefault();
     let number = $("input").val()
     console.log(number);
+    $(".form-div").fadeOut(3000)
+    $("img").fadeOut(3000)
     
   });
 });
