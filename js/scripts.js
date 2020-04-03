14038e89adb4695a33cbe1e013cdbc3416fafa76
josh <jhell85@gmail.com>
2020-04-03 14:01:26 -0700

    added functionality to printText()

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,12 @@ $(document).ready(function() {
     let number = $("input").val()
     console.log(number);
     $(".form-div").fadeOut(3000)
-    $("img").fadeOut(3000)
-    
+    $("img").fadeOut(2000, printText())
   });
 });
+
+function printText() {
+  $('#t').t(`<div id="t">
+ <ins>3</ins>hell world on a pause</ins>
+</div>`)
+}
