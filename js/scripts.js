3f0cd267a201b532792cbef368dbe13f22ae178a
josh <jhell85@gmail.com>
2020-04-03 15:35:40 -0700

    working on branching for numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,40 @@
+// ----------BackEnd----------
+function numberCounter(number){
+ let counter = parseInt(number)
+ let completedArray = []
+ for (let number = 0; number < counter; number++) {
+   numberArray = number.toString().split("")
+   console.log(numberArray.length);
+   
+   for (let i = 0; i < numberArray.length; i++) {
+    intNumber = parseInt(numberArray[i])
+    if (intNumber === 3){
+      completedArray.push(`<li>${number}: Won't you be my neighbor?</li>`)
+    } else if (numberArray.length === (i+2)) {
+      completedArray.push(`<li>${number}</li>`)
+    }
+   }
+ }
+ console.log(completedArray);
+ 
+}
+
+// ----------FrontEnd -----------
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
     let number = $("input").val()
-    console.log(number);
     $(".form-div").fadeOut(3000)
-    $("img").fadeOut(2000, printText())
+    $("img").fadeOut(2000)
+    numberCounter(number)
   });
 });
 
+
 function printText() {
-  $('#t').t(`<div id="t">
- <ins>3</ins>hell world on a pause</ins>
-</div>`)
+  $('#list').t(`
+ <ins>3</ins>
+ <li>Coffee</li>
+ <li>Tea</li>
+ <li>Milk</li>`)
 }
