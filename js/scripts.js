7a4d726f13bca070d3156cd0e409adbb1d057782
josh <jhell85@gmail.com>
2020-04-06 00:18:34 -0700

    added function to print columns only after first column is finished printing

--- js/scripts.js
+++ js/scripts.js
@@ -33,22 +33,49 @@ function checkNumber(numberArray, number) {
 }
 
 // ----------FrontEnd -----------
+
 $(document).ready(function () {
   $("form").submit(function (event) {
     event.preventDefault();
     let number = $("input").val();
-    $(".form-div").fadeOut(2000);
+    $(".form-div").hide();
     $("img").fadeOut(2000);
-    robotArray = numberCounter(number);
-
-    addColumns(robotArray, number);
+    mutatedArray = numberCounter(number);
+    // addColumns(mutatedArray, number);
+    let firstTime = true;
+    addIndividually(mutatedArray, number,firstTime)
   });
 });
 
+function addIndividually(array, number,firstTime){
+let columns = Math.ceil(number / 25);
+if (firstTime){
+  let columnedArray = array.slice(0, 26);
+  array.splice(0, 26);
+  printIndividually(array, columns, columnedArray)
+} else {
+  let columnedArray = array.slice(0, 25);
+      array.splice(0, 25);
+      printIndividually(array, columns, columnedArray)
+}
+function printIndividually(reducingArray, columns, printArray){
+  $("#list").append(`<div id="column${columns}" class="col col-md-3"></div>`);
+  let stringArray = "";
+  printArray.forEach((element)=> {
+    stringArray += element;
+  });
+  if (columns === 1){
+    $(`#column${columns}`).t(stringArray)
+  } else 
+  $(`#column${columns}`).t(stringArray,{fin: function(){
+    addIndividually(reducingArray, columns, false)
+  }})
+}
+}
 function addColumns(array, number) {
   let columns = Math.ceil(number / 25);
   let counter = 1;
-  while (counter < columns + 1) {
+  while (counter < columns + 1 && textFinished) {
     if (counter === 1) {
       let columnedArray = array.slice(0, 26);
       array.splice(0, 26);
@@ -56,19 +83,21 @@ function addColumns(array, number) {
     } else {
       let columnedArray = array.slice(0, 25);
       array.splice(0, 25);
-      printText(columnedArray, counter)
+      printText(columnedArray, counter, )
     }
     counter++;
   }
 }
 
-function printText(array, number) {
+function printText(array, number, reducingArray) {
   $("#list").append(`<div id="column${number}" class="col col-md-3"></div>`);
-  console.log(array);
-
   let stringArray = "";
   array.forEach((element) => {
     stringArray += element;
   });
-  $(`#column${number}`).t(stringArray);
+  $(`#column${number}`).t(stringArray,{fin: function(){textFinishedCallback(number);
+  }});
+}
+function textFinishedCallback (number){
+  console.log(`myCallback${number}`);
 }
